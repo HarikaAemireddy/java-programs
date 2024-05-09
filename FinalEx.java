d28ca8dcde40fd55453540db24e442a234b47740
HarikaAemireddy <169037733+HarikaAemireddy@users.noreply.github.com>
2024-05-09 15:40:24 +0530

    Add files via upload

---
+++ FinalEx.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class FinalEx 
+{
+	
+final int A=1;
+	
+static final int B;
+	
+static 
+{
+		
+System.out.println("Enter number");
+		
+Scanner sc = new Scanner(System.in);
+		
+B=sc.nextInt();
+		
+	
+}
+	
+  
+ void add()
+  
+ {
+	  
+	  
+ System.out.println(A+" "+B);
+   
+}
+	
+public static void main(String[] args)
+ {
+				
+FinalEx f =new FinalEx();
+		
+f.add();
+
+	
+}
+
+
+}
