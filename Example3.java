ae25068ca679471aa93ea6f8b15b44f015dc18a9
HarikaAemireddy <169037733+HarikaAemireddy@users.noreply.github.com>
2024-05-09 15:21:00 +0530

    Add files via upload

---
+++ Example3.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+
+public class Example3 
+{
+
+	
+public static void main(String[] args)
+ {
+		
+int num=5;
+		
+if(num > 10)
+		
+{
+			
+if(num%2==0)
+			
+{
+				
+System.out.println("Satisfied");
+			
+}
+			
+else
+			
+{
+				
+System.out.println("2nd condition failed");
+			
+}
+		
+}
+		
+else
+		
+{
+			
+System.out.println("First condition failed");
+		
+}
+	
+}
+
+
+}
---
+++ conditions.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+
+class Example3
+ {
+
+	
+
+public static void main(String[] args) 
+{
+		
+int age;
+		
+Scanner sc=new Scanner(System.in);
+		
+System.out.println("Enter your age:");
+		
+age=sc.nextInt();
+		
+if(age>=18)
+		
+{
+			
+System.out.println("Eligible for vote");
+		
+}
+		
+else
+		
+{
+			
+System.out.println("Not Eligible for vote");
+		
+}
+	
+}
+
+
+}
