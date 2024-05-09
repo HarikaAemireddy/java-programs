747a4c2bd7c8d09b46a7e4903962affe0302db06
HarikaAemireddy <169037733+HarikaAemireddy@users.noreply.github.com>
2024-05-09 16:15:15 +0530

    Add files via upload

---
+++ Example3a.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+ class Example3
+ {
+
+	
+public static void main(String[] args) 
+{
+    
+ int num,i;
+    
+ boolean f = false;
+     
+System.out.println("Enter a number to check prime or not");
+    
+ Scanner sc = new Scanner(System.in);
+     
+num = sc.nextInt();
+     
+for(i=2; i< num/2;++i)
+    
+ {
+    	 
+if(num%i==0)
+    	 
+{
+    		 
+f=true;
+    		
+ break;
+    	
+ }
+     
+}
+     
+if(f)
+    
+ {
+    	
+ System.out.println("Not a prime");
+   
+  }
+    
+ else
+     
+{
+    	 
+System.out.println("prime");
+    
+ }
+	
+}
+
+}
