563a79c2642de3157e3dd8896dd880870249cbff
HarikaAemireddy <169037733+HarikaAemireddy@users.noreply.github.com>
2024-05-15 07:49:16 +0530

    Add files via upload

---
+++ stringbulder.java
@@ -0,0 +1,23 @@
+ class Example 
+{
+    
+public static void main(String[] args)
+ {
+              
+StringBuilder stringBuilder = new StringBuilder();
+
+              
+ stringBuilder.append("Hello");
+       
+
+ stringBuilder.append(" ");
+       
+ stringBuilder.append("world");
+
+             
+ String result = stringBuilder.toString();
+
+               
+ System.out.println(result);  
+  }
+}
